spall/ChezScheme

add locate-source cache and line+column components to source objects
Add optional beginning-line and beginning-column components to a
source object, so that line and column information can be recorded
independent of the file. Add `locate-source-object-source` to use
the recorded information. Add a cache for `locate-source` as enabled by
the `use-cache?` optional argument, which can avoid compilation times
that are quadratic in the number of `let-values` or `define-values`
forms.

s/primdata.ss
@@ -936,6 +936,8 @@
   (current-exception-state [sig [() -> (exception-state)] [(exception-state) -> (void)]] [flags])
   (current-expand [sig [() -> (procedure)] [(procedure) -> (void)]] [flags])
   (current-input-port [sig [() -> (textual-input-port)] [(textual-input-port) -> (void)]] [flags])   ; not restricted to 1 argument
+  (current-locate-source-object-source [sig [() -> (procedure)] [(procedure) -> (void)]] [flags])
+  (current-make-source-object [sig [() -> (procedure)] [(procedure) -> (void)]] [flags])
   (current-output-port [sig [() -> (textual-output-port)] [(textual-output-port) -> (void)]] [flags])  ; not restricted to 1 argument
   (current-transcoder [sig [() -> (transcoder)] [(transcoder) -> (void)]] [flags])
   (custom-port-buffer-size [sig [() -> (ufixnum)] [(sub-fixnum) -> (void)]] [flags])
@@ -1394,7 +1396,8 @@
   (profile-load-data [sig [(pathname) -> (void)]] [flags true])
   (load-program [sig [(pathname) (pathname procedure) -> (void)]] [flags true])
   (load-shared-object [sig [(maybe-pathname) -> (void)]] [flags true])
-  (locate-source [sig [(sfd uint) -> ()] [(sfd uint) -> (string uint uint)]] [flags])
+  (locate-source [sig [(sfd uint) (sfd uint ptr) -> ()] [(sfd uint) (sfd uint ptr) -> (string uint uint)]] [flags])
+  (locate-source-object-source [sig [(source-object ptr ptr) -> ()] [(source-object ptr ptr) -> (string uint uint)]] [flags])
   (lock-object [sig [(ptr) -> (void)]] [flags unrestricted true])
   (locked-object? [sig [(ptr) -> (boolean)]] [flags unrestricted discard])
   (logand [sig [(sint ...) -> (sint)]] [flags arith-op partial-folder])
@@ -1430,8 +1433,8 @@
   (make-parameter [sig [(ptr) (ptr procedure) -> (procedure)]] [flags true cp02 cp03])
   (make-record-type [sig [(sub-ptr sub-list) (maybe-rtd sub-ptr sub-list) -> (rtd)]] [flags pure alloc cp02])
   (make-source-condition [sig [(ptr) -> (condition)]] [flags pure unrestricted mifoldable discard])
-  (make-source-file-descriptor [sig [(string binary-input-port) (string binary-input-port ptr) -> (sfd)]] [flags true])
-  (make-source-object [sig [(sfd uint uint) -> (source-object)]] [flags pure true mifoldable discard])
+  (make-source-file-descriptor [sig [(string binary-input-port) (string binary-input-port ptr) (string binary-input-port ptr ptr) -> (sfd)]] [flags true])
+  (make-source-object [sig [(sfd uint uint) (sfd uint uint uint uint) -> (source-object)]] [flags pure true mifoldable discard])
   (make-sstats [sig [(time time exact-integer exact-integer time time exact-integer) -> (sstats)]] [flags alloc])
   (make-thread-parameter [feature pthreads] [sig [(ptr) (ptr procedure) -> (ptr)]] [flags true cp02 cp03])
   (make-weak-eq-hashtable [sig [() (uint) -> (eq-hashtable)]] [flags alloc])
@@ -1595,6 +1598,8 @@
   (source-object? [sig [(ptr) -> (boolean)]] [flags pure unrestricted mifoldable discard])
   (source-object-bfp [sig [(source-object) -> (uint)]] [flags pure mifoldable discard])
   (source-object-efp [sig [(source-object) -> (uint)]] [flags pure mifoldable discard])
+  (source-object-bfl [sig [(source-object) -> (ptr)]] [flags pure mifoldable discard])
+  (source-object-bfc [sig [(source-object) -> (ptr)]] [flags pure mifoldable discard])
   (source-object-sfd [sig [(source-object) -> (sfd)]] [flags pure mifoldable discard])
   (sstats-bytes [sig [(sstats) -> (exact-integer)]] [flags mifoldable discard])
   (sstats-cpu [sig [(sstats) -> (time)]] [flags mifoldable discard])
@@ -1720,6 +1725,7 @@
   ($check-heap-errors [flags])
   ($clear-dynamic-closure-counts [flags])  ; added for closure instrumentation
   ($clear-pass-stats [flags])
+  ($clear-source-lines-cache [flags])
   ($close-files [flags])
   ($close-resurrected-files [flags])
   ($close-resurrected-mutexes&conditions [feature pthreads] [flags])
@@ -2232,6 +2238,7 @@
   ($compile-profile [flags])
   ($cp0-inner-unroll-limit #;[sig [() -> (ufixnum)] [(ufixnum) -> (void)]] [flags])
   ($cp0-polyvariant #;[sig [() -> (boolean)] [(ptr) -> (void)]] [flags unrestricted])
+  ($current-mso [flags])
   ($enable-check-heap [flags])
   ($enable-expeditor [feature expeditor] [flags])
   ($enable-pass-timing [flags])

s/syntax.ss
@@ -6416,15 +6416,15 @@
   (lambda (x p wr)
     (define get-source
       (lambda (src)
-        (let ([sfd (source-sfd src)] [fp (source-bfp src)])
-          (call-with-values
-            (lambda () ($locate-source sfd fp))
-            (case-lambda
-              [() (format "[char ~a of ~a]"
-                    fp
-                    (source-file-descriptor-name sfd))]
-              [(path line char)
-               (format "[line ~a, char ~a of ~a]" line char path)])))))
+        (call-with-values
+          (lambda () ((current-locate-source-object-source) src #t #t))
+          (case-lambda
+            [() (let ([sfd (source-sfd src)] [fp (source-bfp src)])
+                  (format "[char ~a of ~a]"
+                          fp
+                          (source-file-descriptor-name sfd)))]
+            [(path line char)
+             (format "[line ~a, char ~a of ~a]" line char path)]))))
     (display "#<syntax " p)
     (wr (syntax->datum x) p)
     (let f ([x x])
@@ -7502,7 +7502,7 @@
         (with-source-path 'include fn
           (lambda (fn)
             (let* ([p ($open-file-input-port 'include fn)]
-                   [sfd ($source-file-descriptor fn p)]
+                   [sfd ($source-file-descriptor fn p #t)]
                    [p (transcoded-port p (current-transcoder))])
               (let ([do-read ($make-read p sfd 0)])
                 (let f ()
@@ -9635,6 +9635,8 @@
 
 (let ()
   (module types (source make-source source? source-sfd source-bfp source-efp
+                 old-source? ; UNPATCHME
+                 source-2d? make-source-2d source-2d-line source-2d-column
                  annotation make-annotation annotation? annotation-expression annotation-source annotation-stripped annotation-flags
                  make-source-file-descriptor source-file-descriptor source-file-descriptor? source-file-descriptor-name
                  source-file-descriptor-length source-file-descriptor-crc
@@ -9665,7 +9667,8 @@
         (wr (%annotation-stripped x) p)
         (display-string ">" p))))
   (set-who! make-source-object
-    (lambda (sfd bfp efp)
+    (case-lambda
+     [(sfd bfp efp)
       (unless (%source-file-descriptor? sfd)
         ($oops who "~s is not a source file descriptor" sfd))
       (unless (if (fixnum? bfp) (fx>= bfp 0) (and (bignum? bfp) ($bigpositive? bfp)))
@@ -9674,10 +9677,30 @@
         ($oops who "~s is not an exact nonnegative integer" efp))
       (unless (<= bfp efp)
         ($oops who "ending file position ~s is less than beginning file position ~s" efp bfp))
-      (%make-source sfd bfp efp)))
+      (%make-source sfd bfp efp)]
+     [(sfd bfp efp line column)
+      (unless (%source-file-descriptor? sfd)
+        ($oops who "~s is not a source file descriptor" sfd))
+      (unless (if (fixnum? bfp) (fx>= bfp 0) (and (bignum? bfp) ($bigpositive? bfp)))
+        ($oops who "~s is not an exact nonnegative integer" bfp))
+      (unless (if (fixnum? efp) (fx>= efp 0) (and (bignum? efp) ($bigpositive? efp)))
+        ($oops who "~s is not an exact nonnegative integer" efp))
+      (unless (if (fixnum? line) (fx>= line 1) (and (bignum? line) ($bigpositive? line)))
+        ($oops who "~s is not an exact positive integer" line))
+      (unless (if (fixnum? column) (fx>= column 1) (and (bignum? column) ($bigpositive? column)))
+        ($oops who "~s is not an exact positive integer" column))
+      (unless (<= bfp efp)
+        ($oops who "ending file position ~s is less than beginning file position ~s" efp bfp))
+      (%make-source-2d sfd bfp efp line column)]))
+  (set-who! current-make-source-object
+    (case-lambda
+     [() (or ($current-mso) make-source-object)]
+     [(x)
+      (unless (procedure? x) ($oops who "~s is not a procedure" x))
+      ($current-mso (if (eq? x make-source-object) #f x))]))
   (set-who! source-object?
     (lambda (x)
-      (%source? x)))
+      (or (%source? x) (%old-source? x)))) ; UNPATCHME
   (set-who! source-object-sfd
     (lambda (x)
       (unless (%source? x) ($oops who "~s is not a source object" x))
@@ -9690,6 +9713,18 @@
     (lambda (x)
       (unless (%source? x) ($oops who "~s is not a source object" x))
       (%source-efp x)))
+  (set-who! source-object-bfl
+    (lambda (x)
+      (cond
+       [(%source-2d? x) (%source-2d-line x)]
+       [(%source? x) #f]
+       [else ($oops who "~s is not a source object" x)])))
+  (set-who! source-object-bfc
+    (lambda (x)
+      (cond
+       [(%source-2d? x) (%source-2d-column x)]
+       [(%source? x) #f]
+       [else ($oops who "~s is not a source object" x)])))
   (set-who! make-annotation
     (case-lambda
       [(expression source stripped)
@@ -9769,10 +9804,36 @@
       (unless (%source-file-descriptor? sfd) ($oops who "~s is not a source-file descriptor" sfd))
       ($open-source-file sfd)))
   (set-who! locate-source
-    (lambda (sfd fp)
-      (unless (%source-file-descriptor? sfd) ($oops who "~s is not a source-file descriptor" sfd))
-      (unless (if (fixnum? fp) (fx>= fp 0) (and (bignum? fp) ($bigpositive? fp))) ($oops who "~s is not an exact nonnegative integer" fp))
-      ($locate-source sfd fp)))
+    (rec locate-source
+      (case-lambda
+       [(sfd fp) (locate-source sfd fp #f)]
+       [(sfd fp use-cache?)
+        (unless (%source-file-descriptor? sfd) ($oops who "~s is not a source-file descriptor" sfd))
+        (unless (if (fixnum? fp) (fx>= fp 0) (and (bignum? fp) ($bigpositive? fp)))
+          ($oops who "~s is not an exact nonnegative integer" fp))
+        ($locate-source sfd fp use-cache?)])))
+  (set-who! locate-source-object-source
+    (lambda (src start? cache?)
+      (cond
+       [(and start?
+             (%source-2d? src))
+        (values (%source-file-descriptor-name (%source-sfd src))
+                (%source-2d-line src)
+                (%source-2d-column src))]
+       [(%source? src)
+        ($locate-source (%source-sfd src)
+                        (if start?
+                            (%source-bfp src)
+                            (%source-efp src))
+                        cache?)]
+       [else
+        ($oops who "~s is not a source object" src)])))
+  (set-who! current-locate-source-object-source
+    ($make-thread-parameter
+     locate-source-object-source
+     (lambda (x)
+       (unless (procedure? x) ($oops who "~s is not a procedure" x))
+       x)))
   (set-who! syntax->annotation
     (lambda (x)
       (cond

amirouche/arew

s/primdata.ss
@@ -936,6 +936,8 @@
   (current-exception-state [sig [() -> (exception-state)] [(exception-state) -> (void)]] [flags])
   (current-expand [sig [() -> (procedure)] [(procedure) -> (void)]] [flags])
   (current-input-port [sig [() -> (textual-input-port)] [(textual-input-port) -> (void)]] [flags])   ; not restricted to 1 argument
+  (current-locate-source-object-source [sig [() -> (procedure)] [(procedure) -> (void)]] [flags])
+  (current-make-source-object [sig [() -> (procedure)] [(procedure) -> (void)]] [flags])
   (current-output-port [sig [() -> (textual-output-port)] [(textual-output-port) -> (void)]] [flags])  ; not restricted to 1 argument
   (current-transcoder [sig [() -> (transcoder)] [(transcoder) -> (void)]] [flags])
   (custom-port-buffer-size [sig [() -> (ufixnum)] [(sub-fixnum) -> (void)]] [flags])
@@ -1394,7 +1396,8 @@
   (profile-load-data [sig [(pathname) -> (void)]] [flags true])
   (load-program [sig [(pathname) (pathname procedure) -> (void)]] [flags true])
   (load-shared-object [sig [(maybe-pathname) -> (void)]] [flags true])
-  (locate-source [sig [(sfd uint) -> ()] [(sfd uint) -> (string uint uint)]] [flags])
+  (locate-source [sig [(sfd uint) (sfd uint ptr) -> ()] [(sfd uint) (sfd uint ptr) -> (string uint uint)]] [flags])
+  (locate-source-object-source [sig [(source-object ptr ptr) -> ()] [(source-object ptr ptr) -> (string uint uint)]] [flags])
   (lock-object [sig [(ptr) -> (void)]] [flags unrestricted true])
   (locked-object? [sig [(ptr) -> (boolean)]] [flags unrestricted discard])
   (logand [sig [(sint ...) -> (sint)]] [flags arith-op partial-folder])
@@ -1430,8 +1433,8 @@
   (make-parameter [sig [(ptr) (ptr procedure) -> (procedure)]] [flags true cp02 cp03])
   (make-record-type [sig [(sub-ptr sub-list) (maybe-rtd sub-ptr sub-list) -> (rtd)]] [flags pure alloc cp02])
   (make-source-condition [sig [(ptr) -> (condition)]] [flags pure unrestricted mifoldable discard])
-  (make-source-file-descriptor [sig [(string binary-input-port) (string binary-input-port ptr) -> (sfd)]] [flags true])
-  (make-source-object [sig [(sfd uint uint) -> (source-object)]] [flags pure true mifoldable discard])
+  (make-source-file-descriptor [sig [(string binary-input-port) (string binary-input-port ptr) (string binary-input-port ptr ptr) -> (sfd)]] [flags true])
+  (make-source-object [sig [(sfd uint uint) (sfd uint uint uint uint) -> (source-object)]] [flags pure true mifoldable discard])
   (make-sstats [sig [(time time exact-integer exact-integer time time exact-integer) -> (sstats)]] [flags alloc])
   (make-thread-parameter [feature pthreads] [sig [(ptr) (ptr procedure) -> (ptr)]] [flags true cp02 cp03])
   (make-weak-eq-hashtable [sig [() (uint) -> (eq-hashtable)]] [flags alloc])
@@ -1594,7 +1597,9 @@
   (source-file-descriptor-path [sig [(sfd) -> (ptr)]] [flags pure mifoldable discard true])
   (source-object? [sig [(ptr) -> (boolean)]] [flags pure unrestricted mifoldable discard])
   (source-object-bfp [sig [(source-object) -> (uint)]] [flags pure mifoldable discard])
+  (source-object-column [sig [(source-object) -> (ptr)]] [flags pure mifoldable discard])
   (source-object-efp [sig [(source-object) -> (uint)]] [flags pure mifoldable discard])
+  (source-object-line [sig [(source-object) -> (ptr)]] [flags pure mifoldable discard])
   (source-object-sfd [sig [(source-object) -> (sfd)]] [flags pure mifoldable discard])
   (sstats-bytes [sig [(sstats) -> (exact-integer)]] [flags mifoldable discard])
   (sstats-cpu [sig [(sstats) -> (time)]] [flags mifoldable discard])
@@ -1720,6 +1725,7 @@
   ($check-heap-errors [flags])
   ($clear-dynamic-closure-counts [flags])  ; added for closure instrumentation
   ($clear-pass-stats [flags])
+  ($clear-source-lines-cache [flags])
   ($close-files [flags])
   ($close-resurrected-files [flags])
   ($close-resurrected-mutexes&conditions [feature pthreads] [flags])
@@ -2232,6 +2238,7 @@
   ($compile-profile [flags])
   ($cp0-inner-unroll-limit #;[sig [() -> (ufixnum)] [(ufixnum) -> (void)]] [flags])
   ($cp0-polyvariant #;[sig [() -> (boolean)] [(ptr) -> (void)]] [flags unrestricted])
+  ($current-mso [flags])
   ($enable-check-heap [flags])
   ($enable-expeditor [feature expeditor] [flags])
   ($enable-pass-timing [flags])

s/syntax.ss
@@ -6416,15 +6416,15 @@
   (lambda (x p wr)
     (define get-source
       (lambda (src)
-        (let ([sfd (source-sfd src)] [fp (source-bfp src)])
-          (call-with-values
-            (lambda () ($locate-source sfd fp))
-            (case-lambda
-              [() (format "[char ~a of ~a]"
-                    fp
-                    (source-file-descriptor-name sfd))]
-              [(path line char)
-               (format "[line ~a, char ~a of ~a]" line char path)])))))
+        (call-with-values
+          (lambda () ((current-locate-source-object-source) src #t #t))
+          (case-lambda
+            [() (let ([sfd (source-sfd src)] [fp (source-bfp src)])
+                  (format "[char ~a of ~a]"
+                          fp
+                          (source-file-descriptor-name sfd)))]
+            [(path line char)
+             (format "[line ~a, char ~a of ~a]" line char path)]))))
     (display "#<syntax " p)
     (wr (syntax->datum x) p)
     (let f ([x x])
@@ -9635,6 +9635,7 @@
 
 (let ()
   (module types (source make-source source? source-sfd source-bfp source-efp
+                 source-2d? make-source-2d source-2d-line source-2d-column
                  annotation make-annotation annotation? annotation-expression annotation-source annotation-stripped annotation-flags
                  make-source-file-descriptor source-file-descriptor source-file-descriptor? source-file-descriptor-name
                  source-file-descriptor-length source-file-descriptor-crc
@@ -9665,7 +9666,8 @@
         (wr (%annotation-stripped x) p)
         (display-string ">" p))))
   (set-who! make-source-object
-    (lambda (sfd bfp efp)
+    (case-lambda
+     [(sfd bfp efp)
       (unless (%source-file-descriptor? sfd)
         ($oops who "~s is not a source file descriptor" sfd))
       (unless (if (fixnum? bfp) (fx>= bfp 0) (and (bignum? bfp) ($bigpositive? bfp)))
@@ -9674,7 +9676,27 @@
         ($oops who "~s is not an exact nonnegative integer" efp))
       (unless (<= bfp efp)
         ($oops who "ending file position ~s is less than beginning file position ~s" efp bfp))
-      (%make-source sfd bfp efp)))
+      (%make-source sfd bfp efp)]
+     [(sfd bfp efp line column)
+      (unless (%source-file-descriptor? sfd)
+        ($oops who "~s is not a source file descriptor" sfd))
+      (unless (if (fixnum? bfp) (fx>= bfp 0) (and (bignum? bfp) ($bigpositive? bfp)))
+        ($oops who "~s is not an exact nonnegative integer" bfp))
+      (unless (if (fixnum? efp) (fx>= efp 0) (and (bignum? efp) ($bigpositive? efp)))
+        ($oops who "~s is not an exact nonnegative integer" efp))
+      (unless (if (fixnum? line) (fx>= line 1) (and (bignum? line) ($bigpositive? line)))
+        ($oops who "~s is not an exact positive integer" line))
+      (unless (if (fixnum? column) (fx>= column 1) (and (bignum? column) ($bigpositive? column)))
+        ($oops who "~s is not an exact positive integer" column))
+      (unless (<= bfp efp)
+        ($oops who "ending file position ~s is less than beginning file position ~s" efp bfp))
+      (%make-source-2d sfd bfp efp line column)]))
+  (set-who! current-make-source-object
+    (case-lambda
+     [() (or ($current-mso) make-source-object)]
+     [(x)
+      (unless (procedure? x) ($oops who "~s is not a procedure" x))
+      ($current-mso (if (eq? x make-source-object) #f x))]))
   (set-who! source-object?
     (lambda (x)
       (%source? x)))
@@ -9690,6 +9712,18 @@
     (lambda (x)
       (unless (%source? x) ($oops who "~s is not a source object" x))
       (%source-efp x)))
+  (set-who! source-object-line
+    (lambda (x)
+      (cond
+       [(%source-2d? x) (%source-2d-line x)]
+       [(%source? x) #f]
+       [else ($oops who "~s is not a source object" x)])))
+  (set-who! source-object-column
+    (lambda (x)
+      (cond
+       [(%source-2d? x) (%source-2d-column x)]
+       [(%source? x) #f]
+       [else ($oops who "~s is not a source object" x)])))
   (set-who! make-annotation
     (case-lambda
       [(expression source stripped)
@@ -9769,10 +9803,36 @@
       (unless (%source-file-descriptor? sfd) ($oops who "~s is not a source-file descriptor" sfd))
       ($open-source-file sfd)))
   (set-who! locate-source
-    (lambda (sfd fp)
-      (unless (%source-file-descriptor? sfd) ($oops who "~s is not a source-file descriptor" sfd))
-      (unless (if (fixnum? fp) (fx>= fp 0) (and (bignum? fp) ($bigpositive? fp))) ($oops who "~s is not an exact nonnegative integer" fp))
-      ($locate-source sfd fp)))
+    (rec locate-source
+      (case-lambda
+       [(sfd fp) (locate-source sfd fp #f)]
+       [(sfd fp use-cache?)
+        (unless (%source-file-descriptor? sfd) ($oops who "~s is not a source-file descriptor" sfd))
+        (unless (if (fixnum? fp) (fx>= fp 0) (and (bignum? fp) ($bigpositive? fp)))
+          ($oops who "~s is not an exact nonnegative integer" fp))
+        ($locate-source sfd fp use-cache?)])))
+  (set-who! locate-source-object-source
+    (lambda (src start? cache?)
+      (cond
+       [(and start?
+             (%source-2d? src))
+        (values (%source-file-descriptor-name (%source-sfd src))
+                (%source-2d-line src)
+                (%source-2d-column src))]
+       [(%source? src)
+        ($locate-source (%source-sfd src)
+                        (if start?
+                            (%source-bfp src)
+                            (%source-efp src))
+                        cache?)]
+       [else
+        ($oops who "~s is not a source object" src)])))
+  (set-who! current-locate-source-object-source
+    ($make-thread-parameter
+     locate-source-object-source
+     (lambda (x)
+       (unless (procedure? x) ($oops who "~s is not a procedure" x))
+       x)))
   (set-who! syntax->annotation
     (lambda (x)
       (cond