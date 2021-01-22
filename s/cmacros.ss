cisco/ChezScheme

updated version to 9.5.4   BUILDING NOTICE makefiles/Mf-install.in makefiles/Makefile-csug.in   scheme.1.in c/Makefile.a6nt c/Makefile.i3nt c/Makefile.ta6nt   c/Makefile.ti3nt mats/Mf-a6nt mats/Mf-i3nt mats/Mf-ta6nt   mats/Mf-ti3nt workarea c/scheme.rc s/7.ss s/cmacros.ss   release_notes/release_notes.stex csug/copyright.stex csug/csug.stex   bintar/Makefile rpm/Makefile pkg/Makefile wininstall/Makefile   wininstall/a6nt.wxs wininstall/i3nt.wxs wininstall/ta6nt.wxs   wininstall/ti3nt.wxs

s/cmacros.ss
@@ -328,7 +328,7 @@
                  [(_ foo e1 e2) e1] ...
                  [(_ bar e1 e2) e2]))))])))
 
-(define-constant scheme-version #x00090503)
+(define-constant scheme-version #x00090504)
 
 (define-syntax define-machine-types
   (lambda (x)

amirouche/arew

s/cmacros.ss
@@ -357,7 +357,7 @@
 ;; ---------------------------------------------------------------------
 ;; Version and machine types:
 
-(define-constant scheme-version #x0905033A)
+(define-constant scheme-version #x09050401)
 
 (define-syntax define-machine-types
   (lambda (x)