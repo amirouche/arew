81ad1238f902118a1968388e54999bed5187b306
Amirouche <amirouche@hyper.dev>
2021-12-26 10:06:07 +0100

    ci: use ubuntu-20.04, use several cores, disable x11, disable curses...
    ... also:
    
    - configure with --kernelobj to make it possible to test the creation
      of executables (default behavior in cisco fork).
    
    - add a workflow file for armv6, that will not trigger on push or
      pull-request;
    
    - disable non-threaded build;
    
    - disable 32bits builds;
    
    - remove arm64 builds because it was using a self-hosted host;

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e -o pipefail
 echo Building boot files...
-./configure --pb -m=$MACH
-make ${MACH}.bootquick
+./configure --pb -m=$MACH --disable-x11 --kernelobj --disable-curses
+make ${MACH}.bootquick -j$(nproc)
 echo Building Chez Scheme...
-./configure -m=$MACH
-make
+./configure -m=$MACH --disable-x11 --kernelobj --disable-curses
+make -j$(nproc)
 case $MACH in
   *a6nt)
     curl -Ls https://github.com/burgerrg/win-iconv/releases/download/v0.0.9/iconv-x64.dll > "$TARGET_MACHINE"/bin/"$TARGET_MACHINE"/iconv.dll
